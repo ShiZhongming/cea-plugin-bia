dbedbf50f6188766173560c72126fbe3a1a96a96
Zhongming Shi <shi@arch.ethz.ch>
2022-03-15 20:44:03 -0600

    bia_crop script initiated

--- bia/bia_dli.py
+++ bia/bia_dli.py
@@ -29,7 +29,7 @@ __credits__ = ["Zhongming Shi"]
 __license__ = "MIT"
 __version__ = "0.1"
 __maintainer__ = "Zhongming Shi"
-__email__ = "shi@arch.ethz.ch"
+__email__ = "cea@arch.ethz.ch"
 __status__ = "Production"
 
 
