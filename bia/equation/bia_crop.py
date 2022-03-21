b441c225d91180457c992dea67bec788216b974d
Zhongming Shi <shi@arch.ethz.ch>
2022-03-20 22:07:50 -0600

    bia-assessment-crop 3

--- bia/equation/bia_crop.py
+++ bia/equation/bia_crop.py
@@ -65,31 +65,9 @@ def calc_crop(locator, config, building_name):
     crop_type = config.agriculture.crop_type
 
     # read crop properties in the BIA database for the selected crop type
-    dir = os.path.dirname(__file__)
-    bia_database_path = os.path.join(dir, "bia_data.xlsx")
-
-    crop_properties = calc_properties_crop_db(bia_database_path, config)
+    crop_properties = calc_properties_crop_db(config)
     # print("gathering the properties of {crop_type}.".format(crop_type=crop_type))
 
-    # unpack the properties of the selected crop type
-    dscp = crop_properties.get('description')   # description of the selected crop type
-    temp_opt_ger_l_c = crop_properties.get('temp_opt_ger_l_c')  # optimal germination Celsius temperature: lower bound
-    temp_opt_ger_u_c = crop_properties.get('temp_opt_ger_u_c')  # optimal germination Celsius temperature: upper bound
-    temp_opt_gro_l_c = crop_properties.get('temp_opt_gro_l_c')  # optimal growth Celsius temperature: lower bound
-    temp_opt_gro_l_c = crop_properties.get('temp_opt_gro_l_c')  # optimal germination Celsius temperature: upper bound
-    temp_por_l_c = crop_properties.get('temp_por_l_c')  # poor plant growth Celsius temperature: lower bound
-    temp_por_u_c = crop_properties.get('temp_por_u_c')  # poor plant growth Celsiustemperature: upper bound
-    cycl_l_day = crop_properties.get('cycl_l_day')  # growth cycle in days: lower bound
-    cycl_u_day = crop_properties.get('cycl_u_day')  # growth cycle in days: upper bound
-    humd_l = crop_properties.get('humd_l')  # suitable humidity: lower bound
-    humd_u = crop_properties.get('humd_u')  # suitable humidity: upper bound
-    dli_l = crop_properties.get('dli_l')    # DLI requirement: lower bound
-    dli_u = crop_properties.get('dli_u')    # DLI requirement: upper bound
-    yld_grd_l_kg_sqm = crop_properties.get('yld_grd_l_kg_sqm')  # yield on ground in kilograms: lower bound
-    yld_grd_u_kg_sqm = crop_properties.get('yld_grd_u_kg_sqm')  # yield on ground in kilograms: upper bound
-    yld_bia_l_kg_sqm = crop_properties.get('yld_bia_l_kg_sqm')  # BIA yield in kilograms: lower bound
-    yld_bia_u_kg_sqm = crop_properties.get('yld_bia_u_kg_sqm')  # BIA yield in kilograms: upper bound
-    mkt_sg_sgd_kg = crop_properties.get('mkt_sg_sgd_kg')    # market price in Singapore: SGD 14.75/kg on 12 Dec 2021 at Lazada
 
     # spot the suitable surfaces for the selected crop type
     # this function calculates the surfaces and days for crop-growing 1 or 0, how many cycles per year on each surface
@@ -124,14 +102,19 @@ def calc_crop(locator, config, building_name):
         pass
 
 
-def calc_properties_crop_db(database_path, config):
+def calc_properties_crop_db(config):
     """
     To retrieve the crop properties stored in the BIA database for the selected crop type.
 
-    :param crop: the selected crop type
-    :type crop_type: string
-    :return: dict with Properties of the crop type taken form the database
+    :param database_path: the selected crop type
+    :type database_path: string
+    :return: dict with properties of the selected crop type retrieved form the database
     """
+
+    # path to the bia database
+    dir = os.path.dirname(__file__)
+    database_path = os.path.join(dir, "bia_data.xlsx")
+
     crop_type = config.agriculture.crop_type
     data = pd.read_excel(database_path, sheet_name="crop")
     crop_properties = data[data['crop_type'] == crop_type].reset_index().T.to_dict()[0]
@@ -139,5 +122,40 @@ def calc_properties_crop_db(database_path, config):
     return crop_properties
 
 def calc_surface_crop_cycle(dli_results, crop_properties, config):
+
+    """
+     To spot the days that are suitable for the selected crop type based on its crop property for each building
+     envelope surface.
+     To calculate the number of growth cycles for the selected crop type for each building envelope surface.
+
+    :param dli_results: the dli on each building envelope surface for 365 days
+    :type dli_results: dataframe
+    :param crop_properties: the crop property of the selected crop type
+    :type crop_properties: dict
+    :return: dataframe of each building envelope surface, the days to be utilised
+    and the number of growth cycles per year
+    """
+
+    # unpack the properties of the selected crop type
+    dscp = crop_properties.get('description')   # description of the selected crop type
+    temp_opt_ger_l_c = crop_properties.get('temp_opt_ger_l_c')  # optimal germination Celsius temperature: lower bound
+    temp_opt_ger_u_c = crop_properties.get('temp_opt_ger_u_c')  # optimal germination Celsius temperature: upper bound
+    temp_opt_gro_l_c = crop_properties.get('temp_opt_gro_l_c')  # optimal growth Celsius temperature: lower bound
+    temp_opt_gro_l_c = crop_properties.get('temp_opt_gro_l_c')  # optimal germination Celsius temperature: upper bound
+    temp_por_l_c = crop_properties.get('temp_por_l_c')  # poor plant growth Celsius temperature: lower bound
+    temp_por_u_c = crop_properties.get('temp_por_u_c')  # poor plant growth Celsius temperature: upper bound
+    cycl_l_day = crop_properties.get('cycl_l_day')  # growth cycle in days: lower bound
+    cycl_u_day = crop_properties.get('cycl_u_day')  # growth cycle in days: upper bound
+    humd_l = crop_properties.get('humd_l')  # suitable humidity: lower bound
+    humd_u = crop_properties.get('humd_u')  # suitable humidity: upper bound
+    dli_l = crop_properties.get('dli_l')    # DLI requirement: lower bound
+    dli_u = crop_properties.get('dli_u')    # DLI requirement: upper bound
+    yld_grd_l_kg_sqm = crop_properties.get('yld_grd_l_kg_sqm')  # yield on ground in kilograms: lower bound
+    yld_grd_u_kg_sqm = crop_properties.get('yld_grd_u_kg_sqm')  # yield on ground in kilograms: upper bound
+    yld_bia_l_kg_sqm = crop_properties.get('yld_bia_l_kg_sqm')  # BIA yield in kilograms: lower bound
+    yld_bia_u_kg_sqm = crop_properties.get('yld_bia_u_kg_sqm')  # BIA yield in kilograms: upper bound
+    mkt_sg_sgd_kg = crop_properties.get('mkt_sg_sgd_kg')    # market price in Singapore: SGD 14.75/kg on 12 Dec 2021 at Lazada
+
+    #
 
     return crop_surface
