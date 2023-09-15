4890cb50367bb8534069f246db7b80ea523f5f33
Zhongming Shi <dutszm@hotmail.com>
2023-09-15 15:51:32 +0200

    fixing warnings

--- bia/bia_plotter.py
+++ bia/bia_plotter.py
@@ -40,7 +40,7 @@ def main(config):
     building_names = config.agriculture.buildings
     num_process = config.get_number_of_processes()
     n = len(building_names)
-    
+
     dir_dli = config.scenario + "/outputs/data/potentials/agriculture/dli"     # path of the directory
     dli_file = os.listdir(dir_dli)  # Getting the list of directories
     if len(dli_file) != n:      # if some or all the DLI results are not in-place
