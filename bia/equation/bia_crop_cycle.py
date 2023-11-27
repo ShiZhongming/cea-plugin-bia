540dff9ac839bd085622a598e9b61a8c58797b79
Zhongming Shi <dutszm@hotmail.com>
2023-11-27 13:12:40 +0100

    update

--- bia/equation/bia_crop_cycle.py
+++ bia/equation/bia_crop_cycle.py
@@ -145,6 +145,10 @@ def calc_crop_cycle(config, building_name, type_crop):
     :type cycl_s_srf: list
 
     """
+    # read config
+    TOLERANCE_TEMP = config.agriculture.temperature_tolerance      # set the tolerance of the preferred temperature against the bounds in bia_data.xlsx
+    greenhouse = config.agriculture.greenhouse
+
     # read the DLI results
     dli_path = config.scenario + "/outputs/data/potentials/agriculture/dli/{building}_DLI.csv"\
         .format(building=building_name)
@@ -154,19 +158,6 @@ def calc_crop_cycle(config, building_name, type_crop):
     # slice the DLIs/temps of the 365 days
     dli_365 = cea_dli_results.loc[:, "0": "364"]
 
-    print("Reading and analysing the weather data.")
-    locator = InputLocator(scenario=config.scenario)
-    weather_data = epwreader.epw_reader(locator.get_weather_file())
-    temp_C_h = weather_data['drybulb_C']
-
-    # calculate from hourly temp_C_h to daily temp_C_d
-    day = pd.Series(range(0, 365))
-    hour_to_day = day.repeat(24).reset_index().pop('index')
-    temp_C_h = pd.merge(temp_C_h, hour_to_day, left_index=True, right_index=True, how="left")
-    temp_C_d = temp_C_h.groupby(['index']).sum().reset_index() / 24
-    del temp_C_d['index']       # average Temperature_C per 365 days
-    temp_C_d = pd.DataFrame(np.repeat(temp_C_d.T.values, n_surface, axis=0), columns=dli_365.columns)
-
     print("Calculating the planting information for {type_crop} on Building {building}."
           .format(type_crop=type_crop, building=building_name))
 
@@ -174,7 +165,6 @@ def calc_crop_cycle(config, building_name, type_crop):
     crop_properties = calc_properties_crop_db(type_crop)
 
     # unpack the properties of the selected crop type: cycle days
-    TOLERANCE_TEMP = 5      # set the tolerance of the preferred temperature against the bounds in bia_data.xlsx
     cycl_i_day = int(crop_properties.get('cycl_i_day'))  # growth cycle in days: initial
     cycl_s_day = int(crop_properties.get('cycl_s_day'))  # growth cycle in days: subsequent
     n_cycl = int(crop_properties.get('n_cycl'))  # number of growth cycles: both initial and subsequent
@@ -190,28 +180,57 @@ def calc_crop_cycle(config, building_name, type_crop):
     temp_criteria_l = temp_opt_gro_l_c      # temperature requirement: lower bound
     temp_criteria_u = temp_opt_gro_u_c      # temperature requirement: upper bound
 
-    # add one cycle at the end of the year, to be linked with Jan 1 plus one cycle
-    dli_365_add_one_cycl = pd.concat([dli_365, dli_365.iloc[:, 0:(cycl_i_day - 1)]], axis=1)
-    temp_c_365_add_one_cycl = pd.concat([temp_C_d, temp_C_d.iloc[:, 0:(cycl_i_day - 1)]], axis=1)
+    if not greenhouse:
+        print("Reading and analysing the weather data.")
+        locator = InputLocator(scenario=config.scenario)
+        weather_data = epwreader.epw_reader(locator.get_weather_file())
+        temp_C_h = weather_data['drybulb_C']
 
-    # spot the first days of potential growth cycles, for the growth of at least one initial cycle only
-    day = []    # to store the first days of potential cycles
-    bool_df = pd.DataFrame()
-    for i in range(365):
-        # for consecutive days equaling a growth cycle:
-        # true if the average DLI is above the required DLI
-        bool_dli = dli_365_add_one_cycl.iloc[:, i:(i + cycl_i_day)].sum(axis=1) >= dli_criteria * cycl_i_day
-        # true if the average temp_C is above the lower bound of preferred temp
-        bool_temp_l = temp_c_365_add_one_cycl.iloc[:, i:(i + cycl_i_day)].sum(axis=1) >= temp_criteria_l * cycl_i_day
-        # true if the average temp_C is below the upper bound of preferred temp
-        bool_temp_u = temp_c_365_add_one_cycl.iloc[:, i:(i + cycl_i_day)].sum(axis=1) <= temp_criteria_u * cycl_i_day
-        # merge criteria
-        mask = [all(tup) for tup in zip(bool_dli, bool_temp_l, bool_temp_u)]
-        bool = pd.Series(mask)
-        bool = bool.to_frame(name=i)
+        # calculate from hourly temp_C_h to daily temp_C_d
+        day = pd.Series(range(0, 365))
+        hour_to_day = day.repeat(24).reset_index().pop('index')
+        temp_C_h = pd.merge(temp_C_h, hour_to_day, left_index=True, right_index=True, how="left")
+        temp_C_d = temp_C_h.groupby(['index']).sum().reset_index() / 24
+        del temp_C_d['index']       # average Temperature_C per 365 days
+        temp_C_d = pd.DataFrame(np.repeat(temp_C_d.T.values, n_surface, axis=0), columns=dli_365.columns)
 
-        # record the boolean values for each day of all surfaces
-        bool_df = pd.concat([bool_df, bool], axis=1)
+        # add one cycle at the end of the year, to be linked with Jan 1 plus one cycle
+        temp_c_365_add_one_cycl = pd.concat([temp_C_d, temp_C_d.iloc[:, 0:(cycl_i_day - 1)]], axis=1)
+        dli_365_add_one_cycl = pd.concat([dli_365, dli_365.iloc[:, 0:(cycl_i_day - 1)]], axis=1)
+
+        # spot the first days of potential growth cycles, for the growth of at least one initial cycle only
+        day = []    # to store the first days of potential cycles
+        bool_df = pd.DataFrame()
+        for i in range(365):
+            # for consecutive days equaling a growth cycle:
+            # true if the average DLI is above the required DLI
+            bool_dli = dli_365_add_one_cycl.iloc[:, i:(i + cycl_i_day)].sum(axis=1) >= dli_criteria * cycl_i_day
+            # true if the average temp_C is above the lower bound of preferred temp
+            bool_temp_l = temp_c_365_add_one_cycl.iloc[:, i:(i + cycl_i_day)].sum(axis=1) >= temp_criteria_l * cycl_i_day
+            # true if the average temp_C is below the upper bound of preferred temp
+            bool_temp_u = temp_c_365_add_one_cycl.iloc[:, i:(i + cycl_i_day)].sum(axis=1) <= temp_criteria_u * cycl_i_day
+            # merge criteria
+            mask = [all(tup) for tup in zip(bool_dli, bool_temp_l, bool_temp_u)]
+            bool = pd.Series(mask)
+            bool = bool.to_frame(name=i)
+
+            # record the boolean values for each day of all surfaces
+            bool_df = pd.concat([bool_df, bool], axis=1)
+
+    else:
+        # add one cycle at the end of the year, to be linked with Jan 1 plus one cycle
+        dli_365_add_one_cycl = pd.concat([dli_365, dli_365.iloc[:, 0:(cycl_i_day - 1)]], axis=1)
+        # spot the first days of potential growth cycles, for the growth of at least one initial cycle only
+        day = []    # to store the first days of potential cycles
+        bool_df = pd.DataFrame()
+        for i in range(365):
+            # for consecutive days equaling a growth cycle:
+            # true if the average DLI is above the required DLI
+            bool = dli_365_add_one_cycl.iloc[:, i:(i + cycl_i_day)].sum(axis=1) >= dli_criteria * cycl_i_day
+            bool = bool.to_frame(name=i)
+
+            # record the boolean values for each day of all surfaces
+            bool_df = pd.concat([bool_df, bool], axis=1)
 
     day_365 = pd.DataFrame(columns=range(365))
     day_365.loc[0] = range(365)
